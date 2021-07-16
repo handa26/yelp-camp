7cf49955e6b0386a83b58d82597ded0ef035ce7e
handa26 <anandamuhammadmtq@gmail.com>
2021-07-16 15:14:59 +0800

    Add basic campgrounds model and seeding it into separate folder

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 const path = require("path");
+
+mongoose.connect("mongodb://localhost:27017/yelpCampDB", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
 
 const app = express();
 
