99c93753ee0c95f594fc7188159775d31f1c941c
handa26 <anandamuhammadmtq@gmail.com>
2021-07-18 13:15:27 +0800

    Modify campground model

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const newCamp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251/1600x900",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, eligendi iure. Quas corrupti incidunt, esse voluptates animi blanditiis dolorem dolor.",
+      price,
     });
 
     await newCamp.save();
