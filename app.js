edc036bc14dc3f512367df99d59de3b98dba041b
handa26 <anandamuhammadmtq@gmail.com>
2021-07-27 16:29:51 +0800

    Breaking out review routes

--- app.js
+++ app.js
@@ -3,16 +3,12 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const ejs = require("ejs");
 const ejsMate = require("ejs-mate");
-const Joi = require("joi");
 const path = require("path");
 
-const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
-const { reviewSchema } = require("./schema");
 
-const Campground = require("./models/campground");
-const Review = require("./models/review");
 const campgrounds = require("./routes/campgrounds");
+const reviews = require("./routes/reviews");
 
 mongoose.connect("mongodb://localhost:27017/yelpCampDB", {
   useNewUrlParser: true,
@@ -34,44 +30,11 @@ app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-const validateReviewData = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
-
 app.use("/campgrounds", campgrounds);
+app.use("/campgrounds/:id/reviews", reviews);
 
 app.get("/", (req, res) => {
   res.render("home");
-});
-
-app.post(
-  "/campgrounds/:id/reviews",
-  validateReviewData,
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-
-    const campground = await Campground.findById(id);
-    const review = new Review(req.body.review);
-
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
-
-app.delete("/campgrounds/:id/reviews/:reviewId", async (req, res) => {
-  const { id, reviewId } = req.params;
-  await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-  await Review.findByIdAndDelete(reviewId);
-
-  res.redirect(`/campgrounds/${id}`);
 });
 
 app.all("*", (req, res, next) => {
