91da89e1757db5d7039857455def0539284a7bac
handa26 <anandamuhammadmtq@gmail.com>
2021-08-04 12:36:35 +0800

    Reviews protected routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,18 +3,20 @@ const express = require("express");
 const catchAsync = require("../utils/catchAsync");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
-const { validateReviewData } = require("../middleware");
+const { validateReviewData, isLoggedIn } = require("../middleware");
 
 const router = express.Router({ mergeParams: true });
 
 router.post(
   "/",
+  isLoggedIn,
   validateReviewData,
   catchAsync(async (req, res) => {
     const { id } = req.params;
 
     const campground = await Campground.findById(id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
 
     campground.reviews.push(review);
     await review.save();
@@ -23,7 +25,7 @@ router.post(
   })
 );
 
-router.delete("/:reviewId", async (req, res) => {
+router.delete("/:reviewId", isLoggedIn, async (req, res) => {
   const { id, reviewId } = req.params;
   await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
   await Review.findByIdAndDelete(reviewId);
