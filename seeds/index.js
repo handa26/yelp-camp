b945c9abc4de6cd30e005397507cc1e26abde888
handa26 <anandamuhammadmtq@gmail.com>
2021-08-11 15:02:22 +0800

    Reseed campgrounds DB

--- seeds/index.js
+++ seeds/index.js
@@ -19,21 +19,37 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  // for (let i = 0; i < 50; i++) {
-  //   const random1000 = Math.floor(Math.random() * 1000);
-  //   const price = Math.floor(Math.random() * 20) + 10;
-  //   const newCamp = new Campground({
-  //     author: "6108b3f3d4f9360194ebb0a9",
-  //     location: `${cities[random1000].city}, ${cities[random1000].state}`,
-  //     title: `${sample(descriptors)} ${sample(places)}`,
-  //     image: "https://source.unsplash.com/collection/483251/1600x900",
-  //     description:
-  //       "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, eligendi iure. Quas corrupti incidunt, esse voluptates animi blanditiis dolorem dolor.",
-  //     price,
-  //   });
+  for (let i = 0; i < 5; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const newCamp = new Campground({
+      author: "610b67f6c9f72e2b507b0779",
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      images: [
+        {
+          url: "https://res.cloudinary.com/ddbesc0xk/image/upload/v1628660651/YelpCamp/auwkzg1b9nffh7kzf7zh.jpg",
+          filename: "YelpCamp/auwkzg1b9nffh7kzf7zh"
+        },
+        {
+          url: "https://res.cloudinary.com/ddbesc0xk/image/upload/v1628660395/YelpCamp/mxtkibq2nvlerwal1jvt.jpg",
+          filename: "YelpCamp/mxtkibq2nvlerwal1jvt"
+        }
+      ],
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, eligendi iure. Quas corrupti incidunt, esse voluptates animi blanditiis dolorem dolor.",
+      price,
+      geometry: {
+        type: "Point",
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude
+        ]
+      }
+    });
 
-  //   await newCamp.save();
-  // }
+    await newCamp.save();
+  }
 };
 
 seedDB().then(() => {
